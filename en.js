57639293a9e81260f95861eafc4ecd33f0117037
LogicalMindApps <46746922+LogicalMindApps@users.noreply.github.com>
2019-11-27 14:41:17 +0300

    Update en.js

--- en.js
+++ en.js
@@ -1,11 +1,13 @@
-strings: {
-    Вход: 'Sign In',
-    Регистрация: 'Sign Up',
-    'Запомнить пароль': 'Save password',
-    'Забыли пароль?': 'Forgot password?',
-    'Язык интерфейса': 'Language',
-    Логин: 'Login',
-    Пароль: 'Password',
-    'logo_pb.svg': 'logo_pb_en.svg',
+{
+    "strings": {
+        "Вход": "Sign In",
+        "Регистрация": "Sign Up",
+        "Запомнить пароль": "Save password",
+        "Забыли пароль?": "Forgot password?",
+        "Язык интерфейса": "Language",
+        "Логин": "Login",
+        "Пароль": "Password",
+        "logo_pb.svg": "logo_pb_en.svg"
+    }
 }
 
