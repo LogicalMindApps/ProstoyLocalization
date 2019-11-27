474da6da247a91019114efd18e38e985f14804cb
LogicalMindApps <46746922+LogicalMindApps@users.noreply.github.com>
2019-11-27 14:33:09 +0300

    Update en.js

--- en.js
+++ en.js
@@ -1,16 +1,11 @@
-{ 
-    strings: {
-        Вход: 'Sign In',
-        Регистрация: 'Sign Up',
-        'Запомнить пароль': 'Save password',
-        'Забыли пароль?': 'Forgot password?',
-        'Язык интерфейса': 'Language',
-        Логин: 'Login',
-        Пароль: 'Password',
-        'logo_pb.svg': 'logo_pb_en.svg',
-    },
-    codes: {
-        404: 'Page not found',
-        'test.error': 'Test error text',
-    }
+strings: {
+    Вход: 'Sign In',
+    Регистрация: 'Sign Up',
+    'Запомнить пароль': 'Save password',
+    'Забыли пароль?': 'Forgot password?',
+    'Язык интерфейса': 'Language',
+    Логин: 'Login',
+    Пароль: 'Password',
+    'logo_pb.svg': 'logo_pb_en.svg',
 }
+
