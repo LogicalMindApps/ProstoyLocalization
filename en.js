88aea6f6b65a9073eb970e519a143e09a7821a03
LogicalMindApps <46746922+LogicalMindApps@users.noreply.github.com>
2019-11-27 14:29:17 +0300

    Update en.js

--- en.js
+++ en.js
@@ -1,17 +1,16 @@
-const enStrings = {
-    Вход: 'Sign In',
-    Регистрация: 'Sign Up',
-    'Запомнить пароль': 'Save password',
-    'Забыли пароль?': 'Forgot password?',
-    'Язык интерфейса': 'Language',
-    Логин: 'Login',
-    Пароль: 'Password',
-    'logo_pb.svg': 'logo_pb_en.svg',
-};
-
-const enCodes = {
-    404: 'Page not found',
-    'test.error': 'Test error text',
-};
-
-export { enStrings, enCodes };
+{ 
+    strings: {
+        Вход: 'Sign In',
+        Регистрация: 'Sign Up',
+        'Запомнить пароль': 'Save password',
+        'Забыли пароль?': 'Forgot password?',
+        'Язык интерфейса': 'Language',
+        Логин: 'Login',
+        Пароль: 'Password',
+        'logo_pb.svg': 'logo_pb_en.svg',
+    },
+    codes: {
+        404: 'Page not found',
+        'test.error': 'Test error text',
+    }
+}
