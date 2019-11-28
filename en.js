8d5eac3353fffa9223047efea0f0a023d305d935
rk <logicalmind.dex@gmail.com>
2019-11-28 10:21:25 +0300

    Тестовый коммит

--- en.js
+++ en.js
@@ -1,17 +1,17 @@
 {
     "strings": {
         "Вход": "Sign In",
-        "Регистрация": "Sign Up",
-        "Запомнить пароль": "Save password",
-        "Забыли пароль?": "Forgot password?",
-        "Язык интерфейса": "Language",
-        "Логин": "Login",
-        "Пароль": "Password",
-        "logo_pb.svg": "logo_pb_en.svg"
+            "Регистрация": "Sign Up",
+            "Запомнить пароль": "Save password",
+            "Забыли пароль?": "Forgot password?",
+            "Язык интерфейса": "Language",
+            "Логин": "Login",
+            "Пароль": "Password",
+            "logo_pb.svg": "logo_pb_en.svg"
     },
     "codes": {
         "404": "Page not found",
-        "test.error": "Test error text"
+            "test.error": "Test error text",
+            "new.code": "New Code"
     }
 }
-
